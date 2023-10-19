9d2ad5bbc88941f891896266134d3c027f11b7ed
jprugel <jonathon.prugel@gmail.com>
2023-10-19 18:33:03 -0400

    Initial Commit

---
+++ src/game_map.rs
@@ -0,0 +1,49 @@
+#[derive(Debug)]
+pub struct GameMap {
+    width: i32,
+    height: i32,
+}
+impl GameMap {
+    pub fn new(width: i32, height: i32) -> Self {
+        Self { width, height }
+    }
+
+    pub fn area(&self) -> i32 {
+        self.width * self.height
+    }
+
+    pub fn to_vec(&self) -> Vec<char> {
+        let vec_size = self.area();
+        let mut result: Vec<char> = vec!['%'; vec_size as usize];
+        for index in 0..vec_size as usize {
+            if index < (self.width as usize) {
+                result[index] = '#';
+                continue;
+            }
+            if index >= (self.area() - (self.width)) as usize {
+                result[index] = '#';
+                continue;
+            }
+            if (index + 1) % (self.width as usize) == 0 || (index + 1) % (self.width as usize) == 1
+            {
+                result[index] = '#';
+                continue;
+            }
+            result[index] = '%'
+        }
+        return result;
+    }
+
+    pub fn print(&self) {
+        let map = self.to_vec();
+        for (index, value) in map.iter().enumerate() {
+            if index == 0 {
+                print!("{}", value);
+            } else if (index + 1) % (self.width as usize) == 0 {
+                println!("{}", value);
+            } else {
+                print!("{}", value);
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+mod game_map;
+
+use crate::game_map::GameMap;
+
+fn main() {
+    let map = GameMap::new(10, 20);
+    map.print();
+}
+
+fn clear_terminal() {
+    // ANSI escape code to clear the screen
+    print!("\x1B[2J\x1B[1;1H"); // This code clears the screen and positions the cursor at (1, 1)
+
+    // Rest of your program goes here
+    println!("This is a cleared terminal screen!");
+}
